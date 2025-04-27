d3dfb98399e3711b6e92335114b8b1456bdf6682
Northumber <northumber82@gmail.com>
2025-04-27 19:49:04 +0200

    add sum integers node

--- __init__.py
+++ __init__.py
@@ -7,16 +7,19 @@ Author: Northumber (Teo Toscano)
 
 from .nodes.from_dir_image_list import LoadImagesFromDirList
 from .nodes.extract_metadata_by_key import ExtractMetadataByKey
+from .nodes.sum_integers import SumIntegers
 
 
 NODE_CLASS_MAPPINGS = {
   "LoadImagesFromDirList": LoadImagesFromDirList,
   "ExtractMetadataByKey": ExtractMetadataByKey,
+  "SumIntegers": SumIntegers,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
   "LoadImagesFromDirList": "Load Image List from Directory",
   "ExtractMetadataByKey": "Extract Metadata value by key",
+  "SumIntegers": "Sum Integers",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
---
+++ nodes/sum_integers.py
@@ -0,0 +1,23 @@
+"""
+    This node sum two integers, can be inputted and outputted as int or string
+"""
+
+
+class SumIntegers:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "int_1": ("INT", {"default": 0, "min": 0, "step": 1}),
+                "int_2": ("INT", {"default": 0, "min": 0, "step": 1}),
+            }
+        }
+
+    RETURN_TYPES = ("INT", "STRING")
+    RETURN_NAMES = ("sum_int", "sum_str")
+    FUNCTION = "sum"
+
+    def sum(self, int_1, int_2):
+        s = int_1 + int_2
+        return (s, str(s))
+
